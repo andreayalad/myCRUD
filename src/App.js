2a6516a5a68d17e3fc2447442660ec15f920ae27
andreayalad <alejandrayala01@gmail.com>
2021-11-20 21:44:08 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import {Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap'
+import { Component } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const url="https://crudcrud.com/api/41d90f227c6843c8ae8e3da5a651e0ce";
+
+class App extends Component {
+state ={
+  data:[]
+}
+  getMethod=()=>{
+    axios.get(url).then(response=>{
+      console.log(response.data);
+    })
+  }
+  componentDidMount(){
+    this.getMethod();
+  }
+  render(){
+    return (
+      <div className="App">
+        <br/>
+        <button className="btn btn-success">Agregar Empresa</button>
+        <br/> <br/>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nombre</th>
+              <th>Pais</th>
+              <th>Capital</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
