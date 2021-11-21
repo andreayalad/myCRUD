a9813ac404589bb59dc3df990fc8c2590e40ed26
andreayalad <alejandrayala01@gmail.com>
2021-11-20 22:46:04 -0600

    Post Method

--- src/App.js
+++ src/App.js
@@ -5,25 +5,57 @@ import {Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap'
 import { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
 
-const url="https://crudcrud.com/api/41d90f227c6843c8ae8e3da5a651e0ce/unicorns";
+const url="https://crudcrud.com/api/0d82f290499e435196b6d650002abc3f/unicorns";
 
 class App extends Component {
 state ={
-  data:[]
+  data:[],
+  modalInsert: false,
+  form:{
+    name:'',
+    age:'',
+    colour:''
+  }
 }
+
   getMethod=()=>{
     axios.get(url).then(response=>{
       this.setState({data: response.data});
     })
   }
+
+  postMethod=async()=>{
+    await axios.post(url, this.state.form).then(response=>{
+      this.modalInsert();
+      this.getMethod();
+    }).catch(error =>{
+      console.log(error.message)
+    })
+  }
+  modalInsert=()=>{
+    this.setState({modalInsert: !this.state.modalInsert});
+  }
+
+  handleChange=async e=>{
+    e.persist();
+    await this.setState({
+      form:{
+        ...this.state.form,
+        [e.target.name]: e.target.value
+      }
+    })
+    console.log(this.state.form);
+  }
+
   componentDidMount(){
     this.getMethod();
   }
   render(){
+    const {form} =this.state;
     return (
       <div className="App">
         <br/>
-        <button className="btn btn-success">Add unicorn</button>
+        <button className="btn btn-success" onClick={()=>this.modalInsert()}>Add unicorn</button>
         <br/> <br/>
         <table className="table">
           <thead>
@@ -45,6 +77,30 @@ state ={
               })}
           </tbody>
         </table>
+        <Modal isOpen={this.state.modalInsert}>
+          <ModalHeader style={{display: 'block'}}>
+              <span style={{float: 'right'}}>x</span>
+          </ModalHeader>
+          <ModalBody>
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input className="form-control" type="text" name="name"  onChange={this.handleChange} value={form.name}/>
+              <br/>
+              <label htmlFor="age">Age</label>
+              <input className="form-control" type="number" name="age"  onChange={this.handleChange} value={form.age}/>
+              <br/>
+              <label htmlFor="colour">Colour</label>
+              <input className="form-control" type="text" name="colour"  onChange={this.handleChange} value={form.colour}/>
+              <br/>
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <button className="btn btn-success" onClick={()=>this.postMethod()}>
+              Add New
+            </button>
+            <button className="btn btn-danger" onClick={()=>this.modalInsert()}>Cancel</button>
+          </ModalFooter>
+        </Modal>
       </div>
     );
   }
